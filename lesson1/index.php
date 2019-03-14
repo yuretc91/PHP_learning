76e7c17700641ae4d6a0434997ee08606792cdd8
Yury Plodunov <yuretc91@gmail.com>
2019-03-14 21:18:50 +0300

    end of first lesson

--- lesson1/index.php
+++ lesson1/index.php
@@ -1,28 +1,25 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="ru">
-
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-	<style>
-
-	</style>
-	
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
-
 <body>
-	<div id="info">This info will change soon</div>
-	<p>My city</p>
-	<input type="text" name="city" id="city">
-	<p>Age</p>
-	<input type="text" name="age" id="age">
-	<button id="btn">Send</button>
-	<p>blablabla</p>
+<h1><?= "Hello, people"?></h1>
+<h2><?php echo "Hello hello hello"?></h2>
+    <?php
+        Print_r("Hello world!");
+        echo "<br>";
 
-	
-
-	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-	<script src="js/script.js"></script>
+        /*egegege
+        egege
+        egegeg*/
+        //wfwwfw
+        echo "Hello world";
+    ?>
 </body>
-
 </html>
+
