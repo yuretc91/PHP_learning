aed5d09b9134de5934c257795b3c11583c76f34d
Yury Plodunov <yuretc91@gmail.com>
2019-03-23 18:04:13 +0300

    require

---
+++ homework2/form.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        form{
+            border: 1px solid black;
+            width: 200px;
+            padding: 0 20px 20px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<form action="serv.php">
+    <p>Name</p>
+    <input type="text">
+    <p>Age</p>
+    <input type="text">
+</form>
+</body>
+</html>
--- homework2/index.php
+++ homework2/index.php
@@ -6,6 +6,20 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        #btnForm{
+            width: 200px;
+            height: 50px;
+            border: none;
+            border-radius: 20px;
+            background: crimson;
+            color: beige;
+            text-align: center;
+            font-size: 2rem;
+            font-family: "segoe ui light";
+            font-weight: 600;
+        }
+    </style>
 </head>
 <body>
 <?php
@@ -15,8 +29,8 @@
 //3. https://slides.com/alex_novikov/deck-10#/
 //
 //Практика:
-//4. При помощи цикла foreach вывести список категорий. Результат Категория 1, Категория 2, и т.д.;
-//5. Написать скрипт вывода формы, в котором задействовать подключение конструкци require_once;
+
+
 //6. Задача. Переменная $lang может принимать два значения: 'ru' и 'en'. Если она имеет значение 'ru',
 //то в переменную $arr запишем массив дней недели на русском языке, а если имеет значение 'en' – то на английском.
 //Решите задачу через 2 if, через switch-case.
@@ -29,18 +43,24 @@
         echo $num . "<br>";
     }
 }*/
+
+
+
+
 //2. При помощи цикла while вывести список категорий. Результат Категория 1, Категория 2, и т.д.;
 /*$numberOfCategory = 5;
 $categoryCount = 1;
 while($categoryCount <= $numberOfCategory){
     echo "Category " . $categoryCount++ . "<br>";
 }*/
+
+
+
 //3. Написать скрипт, который выведет Заголовок, Дату и Имя автора, Картину поста (цикл while);
-$inst = [
-    $socialNetworks = [
+
+    /*$socialNetworks = [
         $inst = [
             "name" => "Instagram",
-            "avtor" => "Kevin Systrom",
             "author" => "Kevin Systrom",
             "date" => "6/10/2010",
             "url" => "https://www.instagram.com/?hl=ru"
@@ -58,9 +78,38 @@ $inst = [
             "url" => "https://vk.com"
         ]
     ];
-$fb = [
-];
+    foreach ($socialNetworks as $network){
+        foreach ($network as $key => $value){
+            echo $key . " - " . $value . "<br>";
+        }
+        echo "<br>";
+    }*/
+
+
+
+//4. При помощи цикла foreach вывести список категорий. Результат Категория 1, Категория 2, и т.д.;
+/*$numberOfCategory = 5;
+
+for ($categoryCount = 1; $categoryCount <= $numberOfCategory; $categoryCount++){
+    echo "Category " . $categoryCount . "<br>";
+}*/
 ?>
+
+
+<!--5. Написать скрипт вывода формы, в котором задействовать подключение конструкци require_once;-->
+<div id="btnForm">
+    Open a form
+</div>
+<?php
+require_once "form.php";
+?>
+
+
+
+
+
+
+
 
 </body>
 </html>
