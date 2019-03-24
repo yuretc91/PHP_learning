1f8b5dc7248d0c86a516e3543ca84c847ec64cec
Yury Plodunov <yuretc91@gmail.com>
2019-03-24 18:38:00 +0300

    lesson4

---
+++ AjaxFirstLesson/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style>
+
+	</style>
+	
+</head>
+
+<body>
+	<div id="info">This info will change soon</div>
+	<p>My city</p>
+	<input type="text" name="city" id="city">
+	<p>Age</p>
+	<input type="text" name="age" id="age">
+	<button id="btn">Send</button>
+	<p>blablabla</p>
+
+	
+
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+	<script src="js/script.js"></script>
+</body>
+
+</html>
