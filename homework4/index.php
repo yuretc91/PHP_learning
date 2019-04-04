c589676a0c2012fb38c9a10ef66c24dde2de310a
Yury Plodunov <yuretc91@gmail.com>
2019-04-04 20:35:10 +0300

    homework4 with ajax

--- homework4/index.php
+++ homework4/index.php
@@ -22,12 +22,13 @@
 
             <form action="serv.php" method="post">
 
-                <input type="text" name="login" class="form-control mb-3" placeholder="login" required>
-                <input type="text" name="password" class="form-control mb-3" placeholder="password" required>
-                <input type="text" name="email" class="form-control mb-3" placeholder="email" required>
+                <input type="text" name="login" class="form-control mb-3" placeholder="login" required id="name">
+<!--                <input type="text" name="password" class="form-control mb-3" placeholder="password" required>-->
+<!--                <input type="text" name="email" class="form-control mb-3" placeholder="email" required>-->
                 <!--<textarea name="loginPassEmail" id="blabla" class="form-control mb-3" placeholder="login password email" required></textarea>-->
-                <input type="submit" name="submit" class="btn btn-block btn-primary">
+                <input type="button" name="submit" class="btn btn-block btn-primary" id="btn">
             </form>
+            <div class="hidden" style="display: none;">SUCCESS</div>
         </div>
     </div>
 </div>
@@ -56,11 +57,29 @@
 
 
 ?>
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
+<script>
+    $( document ).ready(function() {
+        $("#btn").on('click', function() {
+            var name = $('#name').val();
+            $.ajax({
+                url: "serv.php",
+                type: "POST",
+                data: {
+                    name : name
+                }
+            }).done(function(response) {
+                console.log(response);
+                $('.hidden').show();
+            });
+        })
+    });
+
+
+</script>
 </body>
 </html>
 
--- homework4/serv.php
+++ homework4/serv.php
@@ -10,14 +10,17 @@ if (strlen($login) < 3){
 /*$login = explode(" ", $_POST['loginPassEmail'])[0];
 $password = explode(" ", $_POST['loginPassEmail'])[1];
 $email = explode(" ", $_POST['loginPassEmail'])[2];*/
-$login = $_POST['login'];
-$password = $_POST['password'];
-$email = $_POST['email'];
-if (strlen($login) < 3 || strlen($login) > 30){
-    echo "Enter login longer than 3 characters and shorter than 30 characters";
-} elseif(strlen($login) == 0 || strlen($password) == 0 || strlen($email) == 0) {
-    echo "fill in all form fields";
-}else echo "Your login " . $login . "<br>Your password " . $password . "<br>Your email " . $email;
+
+//$password = $_POST['password'];
+//$email = $_POST['email'];
+//if (strlen($login) < 3 || strlen($login) > 30){
+//    echo "Enter login longer than 3 characters and shorter than 30 characters";
+//} elseif(strlen($login) == 0 || strlen($password) == 0 || strlen($email) == 0) {
+//    echo "fill in all form fields";
+//}else echo "Your login " . $login . "<br>Your password " . $password . "<br>Your email " . $email;
+
+$login = $_POST['name'];
 
 
+echo json_encode($login);
 ?>
