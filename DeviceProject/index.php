2cc2561cb03d23bcc5b2da254783aab728e33874
Yury Plodunov <yuretc91@gmail.com>
2019-04-05 17:48:15 +0300

    project device header

---
+++ DeviceProject/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Device</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <header class="container">
+        <ul class="topMiniMenu row">
+            <li class="topMiniMenuItem col-6 search">Поиск по сайту</li>
+            <li class="topMiniMenuItem col-3 user">Войти</li>
+            <li class="topMiniMenuItem col compare">Сравнить</li>
+            <li class="topMiniMenuItem col cart">Корзина</li>
+        </ul>
+        <ul class="topSecondMenu row">
+            <li class="topSecondMenuItem col-6">КАТАЛОГ ТОВАРОВ <img src="icons/plusIcon.png" alt="plus"></li>
+            <li class="topSecondMenuItem col-2">ДОСТАВКА</li>
+            <li class="topSecondMenuItem col-2">ГАРАНТИЯ</li>
+            <li class="topSecondMenuItem col-2">КОНТАКТЫ</li>
+        </ul>
+    </header>
+    <main>
+
+
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+
+</body>
+
+</html>
---
+++ DeviceProject/js/script.js
@@ -0,0 +1,37 @@
+$(function () {
+    /*var main = $("main");
+    var btn = $('.btn');
+    var menu = $('.menu');
+
+
+    $('.btn').each(function (index, value){
+        $(this).on('click', function() {
+            //this.addClass("active");
+            console.log($(this).text());
+
+            $.ajax({
+                url: "serv.php",
+                type: "POST",
+                data: {
+                    btn : $(this).text()
+                }
+            }).done(function(response) {
+                console.log(response);
+                main.html(response);
+            });
+        });
+    });*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
