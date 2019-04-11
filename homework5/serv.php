e42c2c2115859eefb8b9f81484109e13171e989d
Yury Plodunov <yuretc91@gmail.com>
2019-04-11 21:26:37 +0300

    homework5 continuation

--- homework5/serv.php
+++ homework5/serv.php
@@ -1,16 +1,41 @@
 <?php
-if ($_POST["login"] == "" || $_POST["password"] == "" || $_POST["email"] == ""){
-    header('Refresh: 5; url=index.php'); //перенаправление
-    echo "Fill in all form fields. After 5 seconds you will be automatically redirected to re-enter data";
-}elseif (strlen($_POST["login"]) < 3 || strlen($_POST["login"]) > 30){
-    header('Refresh: 5; url=index.php'); //перенаправление
-    echo "Login must be longer than 3 characters and shorter than 30. After 5 seconds you will be automatically redirected to re-enter data";
-}else {file_put_contents('users.txt', 'Login:' . $_POST["login"] . ",password:" . md5($_POST["password"]) .
-    ",e-mail:" . $_POST["email"] . PHP_EOL, FILE_APPEND);
-    echo "Your data has been successfully sent to the server";} //хеширую не логин, а пароль
 
 
 
 
+
+function authorization($userLogin, $userPass, $userEmail){
+    $usersList = file("users.txt");
+
+
+    if ($userLogin == "" || $userPass == "" || $userEmail == ""){
+        header('Refresh: 5; url=index.php'); //перенаправление
+        echo "Fill in all form fields. After 5 seconds you will be automatically redirected to re-enter data";
+    }elseif (strlen($userLogin) < 3 || strlen($userLogin) > 30){
+        header('Refresh: 5; url=index.php'); //перенаправление
+        echo "Login must be longer than 3 characters and shorter than 30. After 5 seconds you will be automatically redirected to re-enter data";
+    }else {
+        foreach ($usersList as $user) {
+            $users[] = explode("%", $user)[0];
+            //$pass = explode("%", $user)[1];
+            //$email = explode("%", $user)[2];
+            //echo "{explode('%', $user)[0])} <br>";
+        }
+        /*foreach ($users as $key){
+            echo $key . "<br>";
+        }*/
+        if (in_array($userLogin, $users) == true) {
+            echo "User with this name is already registered.";
+        } else {
+            file_put_contents('users.txt', $userLogin . "%" . md5($userPass) . "%" . $userEmail . PHP_EOL, FILE_APPEND);
+            echo "Your data has been successfully sent to the server<br>"; //хеширую пароль
+        }
+
+
+
+    }
+}
+
+authorization($_POST["login"], $_POST["password"], $_POST["email"]);
 
 ?>
