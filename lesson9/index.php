ef2f85825f9db2fae501ebb624f5aebc8b8fef3e
Yury Plodunov <yuretc91@gmail.com>
2019-04-14 14:47:33 +0300

    end task from lesson9

--- lesson9/index.php
+++ lesson9/index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!doctype html>
 <html lang="ru">
 <head>
@@ -15,43 +18,53 @@
     }
 
 </style>
+
 <body>
-<!--
+
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <?php
-/*            //форма исчезает после отправки данных
-            if(isset($_POST['submit'])){
-                echo "Your name - " . $_POST["name"];
-            }else {*/?>
             <form action="index.php" method="post">
-                <!--<select name="type" id="">
-                    <option value="text">Text</option>
-                    <option value="email">E-mail</option>
-                    <option value="password">Password</option>
-                </select>-->
-
-                <!--<input type="text" name="name" class="form-control mb-3" placeholder="Enter name" required>
-                <input type="text" name="value" class="form-control mb-3" placeholder="Enter value" required>
-                <input type="submit" name="submit" class="btn btn-block btn-primary">-->
-            <!--</form>
-            <?php /*/*} */?>
+                <input type="text" name="name" class="form-control mb-3" placeholder="Enter name" required>
+                <input type="text" name="country" class="form-control mb-3" placeholder="Enter country" required>
+                <input type="submit" name="submit" class="btn btn-block btn-primary">
+            </form>
+            <a href="secondPage.php" class="btn btn-block btn-primary">Second Page</a>
         </div>
     </div>
-</div>-->
-<nav>
-    <ul>
-        <li><a href="serv.php?page=1">Main page</a></li>
-        <li><a href="serv.php?page=2">Contacts</a></li>
-        <li><a href="serv.php?page=3">News</a></li>
-    </ul>
-</nav>
+</div>
+<?php
+
+if (!isset($_SESSION["enterTime"])){
+    $_SESSION["enterTime"] = date("H:i:s d.m.Y");
+} else {
+    $timeOnline = time() - strtotime($_SESSION["enterTime"]);
+    echo "You visited this site " . $timeOnline . " seconds ago";
+}
+$visit_count = 1;
+if (isset($_SESSION['visit_count'])) {
+    $visit_count = $_SESSION['visit_count'] + 1;
+}
+$_SESSION['visit_count'] = $visit_count;
+$_SESSION["country"] = $_POST["country"];
+$_SESSION["userName"] = $_POST["name"];
+
+?>
 
 
+<?php
 
+//1. по заходу на страницу запишите в сессию текст 'test'. затем обновите страницу и выведите содержимое сессии на экран.
 
+//2. пусть у вас есть две страницы сайта. «апишите на первой странице что-нибудь в сессию, а затем выведите это при заходе на другую страницу.
 
+//3. сделайте счетчик обновлени¤ страницы пользователем. ƒанные храните в сессии. —крипт должен выводить на экран количество обновлений. ѕри первом заходе на страницу он должен вывести сообщение о том, что вы еще не обновл¤ли страницу.
+
+//4. сделайте две страницы: index.php и test.php. ѕри заходе на index.php спросите с помощью формы страну пользовател¤, запишите ее в сессию (форма при этом должна отправитс¤ на эту же страницу). ѕусть затем пользователь зайдет на страницу test.php - выведите там страну пользовател¤.
+
+//5. запишите в сессию врем¤ захода пользовател¤ на сайт. ѕри обновлении страницы выводите сколько секунд назад пользователь зашел на сайт.
+
+?>
 
 
 
--- lesson9/secondPage.php
+++ lesson9/secondPage.php
@@ -1,32 +1,11 @@
-<?php 
-//$login = $_POST["login"];
-//$email = $_POST["email"];
-
-//echo "Registration complete<br>Your login " . $login . "<br>Your email " . $email;
-
-
-/*
-function setInput($type, $name, $value){
-    return "<input type='{$type}' name='{$name}' placeholder='{$value}'";
-}
-
-
-*/?><!--
-<form action="">
-    <?php
-/*    echo setInput($_POST["type"], $_POST["name"], $_POST["value"]);
-    */?>
-</form>-->
-
-
 <?php
-//echo $_GET["page"];
-if ($_GET["page"] == "1"){
-    echo "Main page";
-}elseif ($_GET["page"] == 2){
-    echo "Contacts";
-}elseif ($_GET["page"] == 3){
-    echo "news";
+session_start();
+if (isset($_SESSION["country"]) && isset($_SESSION["userName"])){
+    echo $_SESSION["userName"] . ", your country is " . $_SESSION["country"] . "<br>";
 }
+if (isset($_SESSION["visit_count"])){
+    echo "You have updated the main page " . $_SESSION["visit_count"] . " times";
+}
+
 
 ?>
