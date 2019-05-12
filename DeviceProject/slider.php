ed090a4f8bba833246c75070791c80b09b582dda
Yury Plodunov <yuretc91@gmail.com>
2019-05-12 23:43:05 +0300

    device project BEM start

--- DeviceProject/slider.php
+++ DeviceProject/slider.php
@@ -1,32 +1,34 @@
 
         <section class="slider">
 
-                <div class="slideImage slideActive"><img src="images/slider/slider1.png" alt="slider" ></div>
-                <div class="slideImage"><img src="images/slider/slider2.png" alt="slider" ></div>
-                <div class="slideImage"><img src="images/slider/slider3.png" alt="slider" ></div>
-                <div class="slideAside slideActive">
-                <div class="tagline">Делай селфи, <br> как Бен Стиллер!</div>
-                <div class="slideDescription">Самая длинная палка для селфи доступна в нашем магазине. <br>
+                <div class="slider__slideImage slideActive"><img src="images/slider/slider1.png" alt="slider" ></div>
+                <div class="slider__slideImage"><img src="images/slider/slider2.png" alt="slider" ></div>
+                <div class="slider__slideImage"><img src="images/slider/slider3.png" alt="slider" ></div>
+                <div class="slider__slideAside slideActive">
+                    <div class="slider__slideAside__tagline">Делай селфи, <br> как Бен Стиллер!</div>
+                    <div class="slider__slideAside__slideDescription">Самая длинная палка для селфи доступна в нашем магазине. <br>
                     Восемь (Восемь, Карл!) метров длиной и весом всего 5 килограмм.</div>
-                <div class="sliderButtons">
-                    <div class="sliderInfo"><a href="#">ПОДРОБНЕЕ</a></div>
-                    <div class="sliderIndicator">
-                        <img src="images/slider/sliderButtonFull.png" alt="sliderButtonFull">
-                        <img src="images/slider/sliderButtonEmpty.png" alt="sliderButtonEmpty">
-                        <img src="images/slider/sliderButtonEmpty.png" alt="sliderButtonEmpty">
+                    <div class="slider__slideAside__sliderButtons">
+                        <div class="slider__slideAside__sliderButtons__info"><a href="#">ПОДРОБНЕЕ</a></div>
+                        <div class="slider__slideAside__sliderButtons__indicator">
+                            <img src="images/slider/sliderButtonFull.png" alt="sliderButtonFull">
+                            <img src="images/slider/sliderButtonEmpty.png" alt="sliderButtonEmpty">
+                            <img src="images/slider/sliderButtonEmpty.png" alt="sliderButtonEmpty">
+                        </div>
+                    </div>
+                    <div class="slider__slideAside__slideSpecifications">
+                        <div>8,5 м</div>
+                        <div>5 кг</div>
+                        <div>Карбон</div>
+                    </div>
+                    <div class="slider__slideAside__slideLegend">
+                        <div>Длина палки</div>
+                        <div>Вес палки</div>
+                        <div>Материал</div>
                     </div>
                 </div>
-                <div class="slideSpecifications">
-                    <div>8,5 м</div>
-                    <div>5 кг</div>
-                    <div>Карбон</div>
-                </div>
-                <div class="slideLegend">
-                    <div>Длина палки</div>
-                    <div>Вес палки</div>
-                    <div>Материал</div>
-                </div>
-            </div>
+
+
             <div class="slideAside">
                 <div class="tagline">Худеем <br> правильно!</div>
                 <div class="slideDescription">Самая длинная палка для селфи доступна в нашем магазине. <br>
