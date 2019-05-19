9e7ff3c12e4c564a5b9defa37357cb6fc87913ea
Yury Plodunov <yuretc91@gmail.com>
2019-05-19 16:40:30 +0300

    tasks from lesson14

--- lesson14/index.php
+++ lesson14/index.php
@@ -5,8 +5,26 @@ $is_auth = rand(0, 1);
 
 $user_name = 'Yura'; // укажите здесь ваше имя
 
+//достаём категории
+$getCategories = "SELECT * FROM cathegory";
+$categoriesResult = mysqli_query($connection, $getCategories);
+if(!$categoriesResult) {
+    echo "Запрос не удался";
+}
+
+
+//достаём лоты
+$getLots = "SELECT lots.title, lots.picture, cathegory.name, lots.start_price ";
+$getLots .= "FROM `lots` INNER JOIN `cathegory` ON lots.category_id = cathegory.id";
+$lotsResult = mysqli_query($connection, $getLots);
+if(!$lotsResult) {
+    echo "Запрос не удался";
+}
+
 $content = renderTemplate('index.php', [
-    'user_name' => $user_name
+    'user_name' => $user_name,
+    'categoriesResult' => $categoriesResult,
+    'lotsResult' => $lotsResult
 ]);
 
 print($content);
--- lesson14/templates/index.php
+++ lesson14/templates/index.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Главная</title>
-    <link href="../css/normalize.min.css" rel="stylesheet">
-    <link href="../css/style.css" rel="stylesheet">
+    <link href="css/normalize.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
 <div class="page-wrapper">
@@ -24,7 +24,7 @@
             <nav class="user-menu">
 
                 <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-                <?= $name ?>
+                <?= $user_name ?>
             </nav>
         </div>
     </header>
@@ -36,9 +36,19 @@
             <ul class="promo__list">
 
                 <!--заполните этот список из массива категорий-->
-                <li class="promo__item promo__item--boards">
-                    <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
+                <?php
+
+                foreach ($categoriesResult as $cathegory) :
+
+                ?>
+                <li class="promo__item promo__item--<?= $cathegory['code']?>">
+                    <a class="promo__link" href="pages/all-lots.html"><?= $cathegory['name']?></a>
                 </li>
+                <?php
+
+                endforeach;
+
+                ?>
             </ul>
         </section>
         <section class="lots">
@@ -47,16 +57,21 @@
             </div>
             <ul class="lots__list">
                 <!--заполните этот список из массива с товарами-->
+                <?php
+
+                foreach ($lotsResult as $lots) :
+
+                ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="" width="350" height="260" alt="">
+                        <img src="<?= 'img/' . $lots['picture']?>" width="350" height="260" alt="<?= $lots['title']?>">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category">Название категории</span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                        <span class="lot__category"><?= $lots['name']?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $lots['title']?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
-                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__amount"><?= $lots['start_price']?></span>
                                 <span class="lot__cost">цена<b class="rub">р</b></span>
                             </div>
                             <div class="lot__timer timer">
@@ -65,6 +80,11 @@
                         </div>
                     </div>
                 </li>
+                <?php
+
+                endforeach;
+
+                ?>
             </ul>
         </section>
     </main>
@@ -74,9 +94,19 @@
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
+            <?php
+
+            foreach ($categoriesResult as $cathegory) :
+
+            ?>
             <li class="nav__item">
-                <a href="pages/all-lots.html">Название категории</a>
+                <a href="pages/all-lots.html"><?= $cathegory['name']?></a>
             </li>
+            <?php
+
+            endforeach;
+
+            ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
